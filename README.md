449d4ea0371d6d8291f861610a8a331ef64bd0f9
Steven Lotito <steven.lotito@gmail.com>
2024-12-09 10:40:41 -0800

    day 8 part 2

--- README.md
+++ README.md
@@ -18,9 +18,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 5](./src/bin/05.rs) | `47.8ms` | `-` |
 | [Day 6](./src/bin/06.rs) | `385.8µs` | `-` |
 | [Day 7](./src/bin/07.rs) | `4.0ms` | `308.0ms` |
-| [Day 8](./src/bin/08.rs) | `27.5µs` | `-` |
+| [Day 8](./src/bin/08.rs) | `27.5µs` | `115.8µs` |
 
-**Total: 382.00ms**
+**Total: 382.11ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/08.rs
+++ src/bin/08.rs
@@ -77,7 +77,63 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (grid, antennas) = parse_grid(input);
+
+    let overall: BTreeSet<_> = antennas
+        .values()
+        .map(|points| {
+            points.iter().tuple_combinations().flat_map(|(a, b)| {
+                let (dx, dy) = (a.0.abs_diff(b.0) as isize, a.1.abs_diff(b.1) as isize);
+                let dir = (a.0 - b.0, a.1 - b.1);
+                // all the antennas are now antinodes too...
+                let mut nodes = vec![*a, *b];
+                let mut multiplier = 1;
+                // this is pretty much brute force, going to generate a lot of useless points
+                while multiplier * dx < grid[0].len() as isize
+                    && multiplier * dx >= 0
+                    && multiplier * dy >= 0
+                    && multiplier * dy < grid.len() as isize
+                {
+                    match dir {
+                        (x, y) if x > 0 && y > 0 => {
+                            // a down and right of b
+                            nodes.push((a.0 + multiplier * dx, a.1 + multiplier * dy));
+                            nodes.push((b.0 - multiplier * dx, b.1 - multiplier * dy));
+                        }
+                        (x, y) if x > 0 && y < 0 => {
+                            // a up and right of b
+                            nodes.push((a.0 + multiplier * dx, a.1 - multiplier * dy));
+                            nodes.push((b.0 - multiplier * dx, b.1 + multiplier * dy));
+                        }
+                        (x, y) if x < 0 && y > 0 => {
+                            // a down and left of b
+                            nodes.push((a.0 - multiplier * dx, a.1 + multiplier * dy));
+                            nodes.push((b.0 + multiplier * dx, b.1 - multiplier * dy));
+                        }
+                        (x, y) if x < 0 && y < 0 => {
+                            // a up and left of b
+                            nodes.push((a.0 - multiplier * dx, a.1 - multiplier * dy));
+                            nodes.push((b.0 + multiplier * dx, b.1 + multiplier * dy));
+                        }
+                        _ => unreachable!(),
+                    }
+                    multiplier += 1;
+                }
+                nodes
+            })
+        })
+        .flatten()
+        .collect();
+
+    let antinodes = overall
+        .iter()
+        .filter(|(x, y)| {
+            // filter out ones that are outside the grid boundaries
+            *x >= 0 && *x < grid[0].len() as isize && *y >= 0 && *y < grid.len() as isize
+        })
+        .count() as u32;
+
+    Some(antinodes)
 }
 
 #[cfg(test)]
@@ -93,6 +149,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
