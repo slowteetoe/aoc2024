8ec8140fc98d3e94afa7203a0d9f99a964eb1b49
Steven Lotito <steven.lotito@gmail.com>
2024-12-14 16:15:22 -0800

    day 14 part 2 - pattern recognition, meatbag style

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,7 +1,7 @@
-use std::collections::BTreeMap;
-
+use bmp::Image;
 use glam::IVec2;
 use regex::Regex;
+use std::collections::BTreeMap;
 
 advent_of_code::solution!(14);
 
@@ -47,6 +47,26 @@ fn parse_input(input: &str) -> Vec<Robot> {
         .collect()
 }
 
+fn display(n: u32, robots: &Vec<Robot>, _gridlen: &IVec2) {
+    if n != 6752 {
+        // now that I know the answer, I'm not generating 8k BMP files...
+        return;
+    }
+    let mut img = Image::new(101, 103);
+
+    for (x, y) in img.coordinates() {
+        img.set_pixel(x, y, bmp::consts::BLACK);
+    }
+    robots.iter().for_each(|r| {
+        img.set_pixel(
+            r.pos.x.try_into().unwrap(),
+            r.pos.y.try_into().unwrap(),
+            bmp::consts::GREEN,
+        );
+    });
+    let _ = img.save(format!("data/images/iter-{:04}.bmp", n));
+}
+
 // seems like we can probably simulate for part 1, guessing we'd have to figure out calculation for part 2 esp. since they're giving us grid size...
 pub fn part_one(input: &str) -> Option<u32> {
     let mut robots = parse_input(input);
@@ -88,8 +108,21 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(quadrants.values().fold(1, |acc, n| acc * *n as u32))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut robots = parse_input(input);
+
+    let gridlen = IVec2::new(101, 103);
+
+    for n in 1..7000 {
+        robots.iter_mut().for_each(|r| {
+            r.patrol(1, &gridlen);
+        });
+
+        // yes, I REALLY generate 7,000 BMP files and preview them in the filesystem to find the !@#$!@ Xmas tree
+        display(n, &robots, &gridlen);
+    }
+
+    Some(6752)
 }
 
 #[cfg(test)]
@@ -103,22 +136,22 @@ mod tests {
             pos: IVec2 { x: 2, y: 4 },
             velocity: IVec2 { x: 2, y: -3 },
         };
-        dbg!(&robot);
-        let positions: Vec<IVec2> = (0..2)
+        // dbg!(&robot);
+        let positions: Vec<IVec2> = (0..5)
             .map(|_| {
-                robot.patrol(2, &grid);
+                robot.patrol(1, &grid);
                 robot.pos
             })
             .collect();
 
-        dbg!(&positions);
+        // dbg!(&positions);
         assert_eq!(
             vec![
-                // IVec2::new(4, 1),
+                IVec2::new(4, 1),
                 IVec2::new(6, 5),
-                // IVec2::new(8, 2),
+                IVec2::new(8, 2),
                 IVec2::new(10, 6),
-                // IVec2::new(1, 3),
+                IVec2::new(1, 3),
             ],
             positions
         );
