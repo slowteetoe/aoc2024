79262d7c370bb56a64dd013868c4b12c17aeda09
Steven Lotito <steven.lotito@gmail.com>
2024-12-22 08:53:54 -0800

    use partition_point (binary search) to find the first blocked path

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,7 +1,7 @@
 use glam::IVec2;
 use itertools::Itertools;
 use pathfinding::prelude::astar;
-use tracing::instrument;
+use tracing::{info, instrument};
 
 advent_of_code::solution!(18);
 
@@ -66,9 +66,48 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(solution.expect("should have found a path").1)
 }
 
-#[instrument(skip(_input))]
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+// yeah, I don't feel like figuring this out right now... just brute force (binary search basically)
+#[instrument(skip(input))]
+pub fn part_two(input: &str) -> Option<String> {
+    let known_good = if cfg!(test) { 12 } else { 1024 }; // we know there's a path at least through these because of part 1
+    let input_len = if cfg!(test) { 25 } else { 3450 }; // length of the inputs
+    let grid = parse_input(input);
+    // tracing::debug!("{:?}", &grid.walls[..elapsed]);
+
+    let first_blocked_idx = (known_good..=input_len)
+        .collect_vec()
+        .partition_point(|elapsed| {
+            tracing::info!("checking path after {elapsed}");
+            let solution = astar(
+                &grid.start,
+                |p| {
+                    DIRECTIONS
+                        .iter()
+                        .filter_map(|d| {
+                            let next_pos = p + d;
+                            if (0..=grid.dim).contains(&(next_pos.x as usize))
+                                && (0..=grid.dim).contains(&(next_pos.y as usize))
+                                && !grid.walls[..*elapsed].contains(&next_pos)
+                            {
+                                Some((next_pos, 1))
+                            } else {
+                                None
+                            }
+                        })
+                        .collect_vec()
+                },
+                |_| 0,
+                |p| p == &grid.end,
+            );
+            solution.is_some()
+        });
+    info!(first_blocked_idx, known_good);
+
+    Some(format!(
+        "{},{}",
+        grid.walls[first_blocked_idx + known_good - 1].x,
+        grid.walls[first_blocked_idx + known_good - 1].y
+    ))
 }
 
 #[cfg(test)]
@@ -84,9 +123,10 @@ mod tests {
         assert_eq!(result, Some(22));
     }
 
+    #[traced_test]
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some("6,1".to_owned()));
     }
 }
