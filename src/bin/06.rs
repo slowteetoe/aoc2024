b83c4b76dbc8dad743d895dfff91f8dfe8d3689d
Steven Lotito <steven.lotito@gmail.com>
2024-12-28 17:16:07 -0800

    slow af, but day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,10 +1,11 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeSet;
 
 use itertools::Itertools;
+use tracing::instrument;
 
 advent_of_code::solution!(6);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Dir {
     N = 0,
     S = 1,
@@ -24,22 +25,23 @@ impl Dir {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Guard {
     pos: (usize, usize),
     dir: Dir,
     visited: BTreeSet<(usize, usize, Dir)>,
 }
 
+#[derive(Debug)]
 struct Grid {
-    grid: BTreeMap<(usize, usize), char>,
     dim_x: usize,
     dim_y: usize,
+    walls: BTreeSet<(usize, usize)>, // keep track of wall locations
 }
 
 fn build_grid(input: &str) -> (Guard, Grid) {
     let mut guard = None;
-    let mut grid = BTreeMap::new();
+    let mut walls = BTreeSet::new();
     input.lines().enumerate().for_each(|(row_num, line)| {
         line.chars().enumerate().for_each(|(col_num, c)| {
             if c == '^' {
@@ -49,48 +51,54 @@ fn build_grid(input: &str) -> (Guard, Grid) {
                     visited: BTreeSet::new(),
                 });
             }
-            grid.insert((col_num, row_num), c);
+            if c == '#' {
+                walls.insert((col_num, row_num));
+            }
         });
     });
     (
         guard.unwrap(),
         Grid {
-            grid,
             dim_x: input.lines().next().unwrap().len(),
             dim_y: input.lines().count(),
+            walls,
         },
     )
 }
 
 impl Guard {
-    fn step(&mut self, grid: &Grid) -> Option<((usize, usize), Dir)> {
+    fn step(&mut self, grid: &Grid) -> Result<Option<((usize, usize), Dir)>, ()> {
         // println!("at {:?}  trying to go {:?}", self.pos, self.dir);
         // check bounds
+        if self.visited.contains(&(self.pos.0, self.pos.1, self.dir)) {
+            // println!("loop detected!");
+            return Err(());
+        }
         let next = match self.dir {
             Dir::N => {
                 if self.pos.1 == 0 {
-                    return None;
+                    return Ok(None);
                 } else {
                     Some((self.pos.0, self.pos.1 - 1))
                 }
             }
             Dir::S => {
                 if self.pos.1 == grid.dim_y {
-                    return None;
+                    return Ok(None);
                 } else {
                     Some((self.pos.0, self.pos.1 + 1))
                 }
             }
             Dir::E => {
-                if self.pos.1 == grid.dim_x {
-                    return None;
+                if self.pos.0 == grid.dim_x {
+                    return Ok(None);
                 } else {
                     Some((self.pos.0 + 1, self.pos.1))
                 }
             }
             Dir::W => {
-                if self.pos.1 == 0 {
-                    return None;
+                if self.pos.0 == 0 {
+                    return Ok(None);
                 } else {
                     Some((self.pos.0 - 1, self.pos.1))
                 }
@@ -100,21 +108,20 @@ impl Guard {
         // insert here so we don't have to subtract out the exit
         self.visited.insert((self.pos.0, self.pos.1, self.dir));
 
-        if grid.grid.get(&next) == Some(&'#') {
+        if grid.walls.contains(&next) {
+            // println!("hit a wall, turning");
             self.dir = self.dir.perp();
-            return Some((next, self.dir));
+            return Ok(Some((next, self.dir)));
         }
         self.pos = next;
-        Some((next, self.dir))
+        Ok(Some((next, self.dir)))
     }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (mut guard, grid) = build_grid(input);
 
-    while let Some(_next) = guard.step(&grid) {
-        // println!("went to {},{}", next.0, next.1);
-    }
+    while let Ok(Some(_next)) = guard.step(&grid) {}
 
     let unique = guard
         .visited
@@ -123,26 +130,57 @@ pub fn part_one(input: &str) -> Option<u32> {
         .unique()
         .count();
 
-    // off by one somewhere - solution needs 5404 but gets 5403 unless I add in 1 (but example gets 42 if add in 1, and should be 41)
     Some(unique as u32)
 }
 
-// 16k empty spaces, so can't just randomly pick one... have to be smarter
-// actually since the solution to part one was 5404, only that many to randomly choose from
-// p1 kept track of locations visited, but not direction - since we're looking for loops, will need to take
-// direction into account also I think...
+#[instrument(skip(input))]
 pub fn part_two(input: &str) -> Option<u32> {
-    let (mut guard, grid) = build_grid(input);
+    let (guard, mut grid) = build_grid(input);
 
-    while let Some(_next) = guard.step(&grid) {
-        // println!("went to {},{}", next.0, next.1);
-    }
+    // ugly, but now need to differentiate between a successful step (which will be Ok(Some(_))), a successful exit (which will be Ok(None)) and a loop (which will be Err())
+    // run through once to get the valid locations
+    let mut pristine = guard.clone();
+    while let Ok(Some(_next)) = pristine.step(&grid) {}
 
-    None
+    // We now know all the possible locations for a new wall, see if adding exactly one creates a loop
+    // debug!(?pristine.visited);
+
+    let loops = pristine
+        .visited
+        .iter()
+        .map(|(x, y, _)| (x, y))
+        .unique()
+        .map(|(x, y)| {
+            let new_wall = (x.clone(), y.clone());
+            // debug!("Introducing wall at {:?}", new_wall);
+            let mut loop_detected = false;
+            let mut exited = false;
+            let mut new_guard = guard.clone();
+            grid.walls.insert(new_wall);
+            while !loop_detected && !exited {
+                match new_guard.step(&grid) {
+                    Ok(Some(_)) => (),
+                    Ok(None) => {
+                        exited = true;
+                    }
+                    Err(_) => {
+                        loop_detected = true;
+                    }
+                }
+            }
+            grid.walls.remove(&new_wall);
+            loop_detected
+        })
+        .filter(|v| *v == true)
+        .collect_vec();
+
+    Some(loops.len() as u32)
 }
 
 #[cfg(test)]
 mod tests {
+    use tracing_test::traced_test;
+
     use super::*;
 
     #[test]
@@ -151,6 +189,7 @@ mod tests {
         assert_eq!(result, Some(41));
     }
 
+    #[traced_test]
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
