630b78930bbe727fef81906ac9de2f003710a80d
Steven Lotito <steven.lotito@gmail.com>
2024-12-30 13:04:32 -0800

    day 24 part 1

--- README.md
+++ README.md
@@ -33,9 +33,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 20](./src/bin/20.rs) | `8.2s` | `-` |
 | [Day 22](./src/bin/22.rs) | `808.6µs` | `-` |
 | [Day 23](./src/bin/23.rs) | `4.5ms` | `-` |
+| [Day 24](./src/bin/24.rs) | `314.4µs` | `-` |
 | [Day 25](./src/bin/25.rs) | `228.1µs` | `-` |
 
-**Total: 11496.26ms**
+**Total: 11496.57ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,7 +1,98 @@
+use std::collections::{BTreeMap, VecDeque};
+
+use itertools::Itertools;
+use tracing::debug;
+
 advent_of_code::solution!(24);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Logic {
+    And,
+    Or,
+    Xor,
+}
+
+#[derive(Debug)]
+struct Gate<'a> {
+    logic: Logic,
+    a: &'a str,
+    b: &'a str,
+    out: &'a str,
+}
+
+fn parse_input(input: &str) -> (BTreeMap<&str, bool>, Vec<Gate>) {
+    let (wires, gates) = input.split_once("\n\n").expect("two sections");
+    let wires: BTreeMap<_, _> = wires
+        .lines()
+        .map(|line| {
+            let (name, val) = line.split_once(": ").unwrap();
+            (name, val == "1")
+        })
+        .collect();
+
+    let gates = gates
+        .lines()
+        .map(|line| {
+            // gate logic gate -> out
+            let (left, out) = line.split_once(" -> ").expect("two gate parts");
+            let left = left.splitn(3, " ").collect_vec();
+            Gate {
+                logic: match left[1] {
+                    "AND" => Logic::And,
+                    "OR" => Logic::Or,
+                    "XOR" => Logic::Xor,
+                    _ => unreachable!(),
+                },
+                a: left[0],
+                b: left[2],
+                out,
+            }
+        })
+        .collect_vec();
+
+    debug!(?wires, ?gates);
+    (wires, gates)
+}
+
+fn simulate(wires: &BTreeMap<&str, bool>, gates: &Vec<Gate>) -> u64 {
+    let mut wires = wires.clone();
+    let mut queue = VecDeque::from_iter(gates.iter());
+
+    while let Some(gate) = queue.pop_front() {
+        match (wires.get(gate.a), wires.get(gate.b)) {
+            (Some(&a), Some(&b)) => {
+                // compute and "write" to output wire
+                let result = match gate.logic {
+                    Logic::And => a && b,
+                    Logic::Or => a || b,
+                    Logic::Xor => a ^ b,
+                };
+                wires.insert(gate.out, result);
+            }
+            _ => {
+                // println!("{:?} not ready yet", &gate);
+                //push back onto queue if one or more wires values aren't available yet
+                queue.push_back(gate);
+            }
+        }
+    }
+    let final_bin = wires
+        .iter()
+        .filter(|w| w.0.starts_with("z"))
+        .sorted()
+        .rev()
+        .map(|v| if *v.1 { "1" } else { "0" })
+        .join("");
+    let output = u64::from_str_radix(&final_bin, 2).expect("valid binary");
+    debug!(final_bin, output);
+    output
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (wires, gates) = parse_input(input);
+    let result = simulate(&wires, &gates);
+    // println!("{:?}", result);
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -10,12 +101,15 @@ pub fn part_two(input: &str) -> Option<u32> {
 
 #[cfg(test)]
 mod tests {
+    use tracing_test::traced_test;
+
     use super::*;
 
+    #[traced_test]
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 
     #[test]
